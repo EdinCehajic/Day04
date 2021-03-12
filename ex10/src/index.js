ef0a674f795b60673dc8cb9129e7c1cd378a7b31
EdinCehajic <ed.cehajic@gmail.com>
2021-03-12 07:39:35 +0100

    Adding Day04

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne, numTwo){
+    
+    // Only change code below this line
+    var myFirstName = "Edo";
+    var myLastName = "Cehajic"
+    var myHomeTown = "Visoko"
+    // Only change code above this line
+    
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
